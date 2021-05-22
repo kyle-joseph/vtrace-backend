c8cc6220f49a0933f95f8a70e02295d245cc9273
Kyle Joseph Timajo <timajokyle24@gmail.com>
2021-05-23 01:29:11 +0800

    Removed trust proxy

--- app.js
+++ app.js
@@ -17,39 +17,9 @@ var establishmentsRouter = require("./routes/establishment")
 
 var app = express()
 
-// Add headers
-// app.use(function (req, res, next) {
-//     // Website you wish to allow to connect
-//     res.setHeader(
-//         "Access-Control-Allow-Origin",
-//         "https://kyle-joseph.github.io"
-//     )
-
-//     // Request methods you wish to allow
-//     res.setHeader(
-//         "Access-Control-Allow-Methods",
-//         "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//     )
-
-//     // Request headers you wish to allow
-//     res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "X-Requested-With,content-type"
-//     )
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader("Access-Control-Allow-Credentials", true)
-
-//     // Pass to next layer of middleware
-//     next()
-// })
-
-app.enable("trust proxy")
-
 app.use(
     cors({
-        credentials: true,
+        // credentials: true,
         origin: [
             "http://localhost:3000",
             "http://localhost:3002",
