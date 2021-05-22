ed59b565e0e54d3af52473da65238200e7330b51
Kyle Joseph Timajo <timajokyle24@gmail.com>
2021-05-23 00:52:57 +0800

    Removed trust proxy

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ var establishmentsRouter = require("./routes/establishment")
 
 var app = express()
 
-app.enable("trust proxy")
-
 // Add headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
